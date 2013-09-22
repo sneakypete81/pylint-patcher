fc354359835f268db275b2faae94e641d4d1563e
sneakypete81 <sneakypete81@gmail.com>
2013-09-22 16:11:51 +0100

    Update cli script to handle cases where a target is not provided. Added cli unit tests.

--- pylint_patcher/main.py
+++ pylint_patcher/main.py
@@ -3,25 +3,36 @@
 main.py : Pylint Patcher main console script
 """
 import sys
+import argparse
 import pylint.lint
 from pylint_patcher import patcher
 
-def main(args=sys.argv[1:], **kwds):
+class ArgumentParser(argparse.ArgumentParser):
+    def error(self, message):
+        """Override the error handler to ignore all errors"""
+        pass
+
+def main(args=sys.argv[1:]):
     """
     Apply the patchfile containing the pylint ignores,
     then run the linter,
     then revert the patchfile changes.
     """
-    if args == []:
-        # Print the Pylint usage docs
-        pylint.lint.Run(args, **kwds)
+    # Attempt to parse the Pylint target
+    parser = ArgumentParser(add_help=False)
+    parser.add_argument("target")
+    parsed_args, _ = parser.parse_known_args(args)
+
+    if parsed_args.target is None:
+        # No target, so just print the Pylint usage docs
+        pylint.lint.Run(args)
         return
 
     # Apply the ignore patchfile before linting
-    patch = patcher.Patcher(args[0])
+    patch = patcher.Patcher(parsed_args.target)
     patch.patch()
     try:
-        pylint.lint.Run(args, **kwds)
+        pylint.lint.Run(args)
     finally:
         # Revert the ignore patchfile
         patch.unpatch()
---
+++ tests/test_cli.py
@@ -0,0 +1,37 @@
+import unittest
+import mock
+import pylint_patcher.main
+
+class Cli(unittest.TestCase):
+    @mock.patch.object(pylint_patcher.patcher, "Patcher")
+    @mock.patch.object(pylint_patcher.main.pylint.lint, "Run")
+    def test_no_args(self, mock_run, mock_patcher):
+        """
+        If no arguments/options are provided, check that only pylint is
+        called.
+        """
+        pylint_patcher.main.main([])
+        mock_run.assert_called_with([])
+        self.assertFalse(mock_patcher.called)
+
+    @mock.patch.object(pylint_patcher.patcher, "Patcher")
+    @mock.patch.object(pylint_patcher.main.pylint.lint, "Run")
+    def test_help_option_is_passed_to_pylint(self, mock_run, mock_patcher):
+        """
+        If a single option is provided, check that only pylint is
+        called.
+        """
+        pylint_patcher.main.main(["--help"])
+        mock_run.assert_called_with(["--help"])
+        self.assertFalse(mock_patcher.called)
+
+    @mock.patch.object(pylint_patcher.patcher, "Patcher")
+    @mock.patch.object(pylint_patcher.main.pylint.lint, "Run")
+    def test_with_target(self, mock_run, mock_patcher):
+        """
+        If a target argument is provided, check that both patch and
+        pylint are called.
+        """
+        pylint_patcher.main.main(["target", "--option"])
+        mock_run.assert_called_with(["target", "--option"])
+        mock_patcher.assert_called_with("target")
